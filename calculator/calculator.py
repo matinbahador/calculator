f0967206c978dcda09a0aef7dab7c3288c6e20b0
matin <matin.shakeri@icloud.com>
2023-06-27 21:03:11 +0330

    condition's

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -21,3 +21,17 @@ def power(a, b):
 a = int(input("Enter your first number: "))
 req = input("Enter oprator (+ , - , * , / , ** ): ")
 b = int(input("Enter your second number: "))
+
+if req == "+":
+    result = add(a, b)
+elif req == "-":
+    result = sub(a, b)
+elif req == "*":
+    result = mult(a, b)
+elif req == "/":
+    result = div(a, b)
+elif req == "**":
+    result = power(a, b)
+else:
+    print("Invalid operator")
+    result = None
